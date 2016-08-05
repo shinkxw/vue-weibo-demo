1093e4444deea4390062f737da647fcd9750caf4
kanxinwei <shink_7770@qq.com>
2016-08-05 14:06:56 +0800

    utils增加jwt解码方法

--- src/libs/utils.js
+++ src/libs/utils.js
@@ -88,7 +88,41 @@ const md5 = function(data) {
   return crypto.createHash("md5").update(str, 'utf8').digest("hex");
 }
 
+
+const JwtDecode = function (token) {
+  var segments = token.split('.');
+
+  if (segments.length !== 3) {
+    throw new Error('Not enough or too many segments');
+  }
+
+  // All segment should be base64
+  var headerSeg = segments[0];
+  var payloadSeg = segments[1];
+  var signatureSeg = segments[2];
+
+  // base64 decode and parse JSON
+  var header = JSON.parse(base64urlDecode(headerSeg));
+  var payload = JSON.parse(base64urlDecode(payloadSeg));
+
+  return {
+    header: header,
+    payload: payload,
+    signature: signatureSeg
+  }
+}
+
+function base64urlDecode(str) {
+  return new Buffer(base64urlUnescape(str), 'base64').toString();
+};
+
+function base64urlUnescape(str) {
+  str += Array(5 - str.length % 4).join('=');
+  return str.replace(/\-/g, '+').replace(/_/g, '/');
+}
+
 exports.getCheck = getCheck;
 exports.fmtDate = fmtDate;
 exports.MillisecondToDate = MillisecondToDate;
 exports.md5 = md5;
+exports.JwtDecode = JwtDecode;
