a6cf6d1c8be637ac0be6b2d1cf276a27c4580195
kanxinwei <shink_7770@qq.com>
2016-08-05 14:05:53 +0800

    使用vue_resource的root选项代替root_path

--- src/resources.js
+++ src/resources.js
@@ -1,5 +1,6 @@
 module.exports = function(Vue){
-  let root_path = 'http://localhost:3000/api/v1/'
-  window.user_resource = Vue.resource(root_path + 'users{/id}')
-  window.micropost_resource = Vue.resource(root_path + 'microposts{/id}')
+  Vue.http.options.root = 'http://localhost:3000/api/v1'
+
+  window.user_resource = Vue.resource('users{/id}', {}, userActions)
+  window.micropost_resource = Vue.resource('microposts{/id}')
 }
