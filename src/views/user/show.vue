5c3dbb58807f400c996b1539c9fe2fcd7fe5e55a
kanxinwei <shink_7770@qq.com>
2016-08-12 18:14:28 +0800

    用户页面增加微博显示

--- src/views/user/show.vue
+++ src/views/user/show.vue
@@ -8,6 +8,26 @@
         </h1>
       </section>
     </aside>
+    <div class="col-md-8">
+      <template v-if="get_user && (micropost_all_count > 0)">
+        <h3>微博 ({{ micropost_all_count }})</h3>
+        <ol class="microposts">
+          <li v-for="micropost of microposts" id="micropost-{{ micropost.id }}">
+            <gravatar :alt="user.name" :email="user.email" size="50"></gravatar>
+            <span class="user">
+              <a v-link="{ name: 'user_show', params: { id: micropost.user_id } }">
+                {{ user.name }}
+              </a>
+            </span>
+            <span class="content">{{ micropost.content }}</span>
+            <span class="timestamp">
+              {{ micropost.created_at | getLastTimeStr true }} 发布
+            </span>
+          </li>
+        </ol>
+        <paginate :resource="micropost_resource" :param="paginate_param"></paginate>
+      </template>
+    </div>
   </div>
 </template>
 
@@ -15,15 +35,30 @@
   export default {
     data() {
       return {
-        user: {}
+        user: {},
+        get_user: false,
+        microposts: [],
+        micropost_all_count: 1,
+        paginate_param: {},
+        micropost_resource: micropost_resource
       }
     },
     route:{
       data(transition) {
         let uid = transition.to.params.id
         return {
-          user: user_resource.get({id: uid}).then((res) => { return res.json() })
+          user: user_resource.get({id: uid}).then((res) => { return res.json() }),
+          paginate_param: {user_id: uid},
+          get_user: true
         }
+      }
+    },
+    events: {
+      // 分页组件传回的表格数据
+      'data' (res) {
+        this.microposts = res.data
+        this.micropost_all_count = res.all_count
+        window.scrollTo(0, 0)
       }
     }
   }
