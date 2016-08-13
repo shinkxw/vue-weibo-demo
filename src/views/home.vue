1466dc258d878aad2e72e16e0f88b10a1d9e3c3b
kanxinwei <shink_7770@qq.com>
2016-08-13 10:31:45 +0800

    修复错句

--- src/views/home.vue
+++ src/views/home.vue
@@ -2,7 +2,7 @@
   <div class="center jumbotron">
     <h1>欢迎来到 Vue weibo demo</h1>
     <h2>
-      这个是一个使用 <a href="http://cn.vuejs.org/">Vue.js</a> 搭建的应用
+      这是一个使用 <a href="http://cn.vuejs.org/">Vue.js</a> 搭建的应用
     </h2>
     <a class="btn btn-lg btn-primary" v-link="{ name: 'user_signup' }">现在注册!</a>
   </div>
